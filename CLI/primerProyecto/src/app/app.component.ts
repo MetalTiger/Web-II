47d1aa7060a600bc40c29c630901a1a133b35291
Kevin <kevinkamacho47@gmail.com>
2021-09-28 14:03:11 -0600

    Cambiar Nombre primerProyecto

--- CLI/primerProyecto/src/app/app.component.ts
+++ CLI/primerProyecto/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Proyecto Final a la Vista';
+  title = 'PrimerProyecto';
 }
