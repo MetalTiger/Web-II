48af915acc754f8c6340db3bee3623419bd8a9d5
Kevin <kevinkamacho47@gmail.com>
2021-10-28 19:40:13 -0600

    Habilitar modo fuera de linea

--- CLI/weather-app/src/app/app.module.ts
+++ CLI/weather-app/src/app/app.module.ts
@@ -6,7 +6,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WeatherComponent } from './weather/weather.component';
 import { MatCardModule } from '@angular/material/card'
 import { MatIconModule } from '@angular/material/icon'
-import { MatInputModule } from '@angular/material/input'
+import { MatInputModule } from '@angular/material/input';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [
@@ -19,7 +21,13 @@ import { MatInputModule } from '@angular/material/input'
     HttpClientModule,
     MatCardModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
