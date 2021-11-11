cd53c88249dcc49f796302021489fd8b4329f4b8
Kevin <kevinkamacho47@gmail.com>
2021-11-11 14:59:01 -0700

    Añadir ngx-wig

--- CLI/my-editor/src/app/app.module.ts
+++ CLI/my-editor/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgxWigModule } from 'ngx-wig';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxWigModule
   ],
   providers: [],
   bootstrap: [AppComponent]
