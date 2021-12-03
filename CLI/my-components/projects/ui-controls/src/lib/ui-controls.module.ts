b506a620deb291ae17ee37df759f9638e4050c93
Kevin <kevinkamacho47@gmail.com>
2021-12-03 12:59:58 -0700

    Visualizar datos en la tarjeta

--- CLI/my-components/projects/ui-controls/src/lib/ui-controls.module.ts
+++ CLI/my-components/projects/ui-controls/src/lib/ui-controls.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { UiControlsComponent } from './ui-controls.component';
-
-
+import { CardListComponent } from './card-list/card-list.component';
+import { CommonModule } from "@angular/common";
 
 @NgModule({
   declarations: [
-    UiControlsComponent
+    UiControlsComponent,
+    CardListComponent
   ],
   imports: [
+    CommonModule  // Necesaria para la directiva *ngFor
   ],
   exports: [
-    UiControlsComponent
+    UiControlsComponent,
+    CardListComponent
   ]
 })
 export class UiControlsModule { }
